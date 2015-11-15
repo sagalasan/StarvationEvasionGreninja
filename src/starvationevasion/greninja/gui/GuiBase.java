6cc8789a8f85029dd0a220c7f0e39d40f8e09ddf
JJ505 <jjohnson101j@gmail.com>
2015-11-14 17:19:14 -0700

    testing that I set git up correctly

--- src/starvationevasion/greninja/gui/GuiBase.java
+++ src/starvationevasion/greninja/gui/GuiBase.java
@@ -14,6 +14,7 @@ import starvationevasion.greninja.gui.basePane.*;
  * All user input and output is routed through this class.
  * @author Justin Thomas jthomas105@unm.edu
  */
+//test to get git working
 public class GuiBase extends Application
 {
   private PolicyPane policyPane;
