6508c0c473a473a07d07065965fa48e9327fe515
christiaan <cmdm@unm.edu>
2015-11-16 21:10:44 -0700

    Added more specific implementation

--- src/starvationevasion/greninja/serverCom/threads/ServerWriter.java
+++ src/starvationevasion/greninja/serverCom/threads/ServerWriter.java
@@ -3,6 +3,8 @@ package starvationevasion.greninja.serverCom.threads;
 import starvationevasion.greninja.serverCom.ServerConnection;
 
 import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Created by sagalasan on 11/14/15.
@@ -12,10 +14,21 @@ public class ServerWriter extends Thread
   private ServerConnection serverConnection;
   private PrintWriter writer;
 
+  private boolean needsToRun = false;
+
+  private LinkedList<String> messageQueue;
+
   public ServerWriter(ServerConnection serverConnection, PrintWriter writer)
   {
     this.serverConnection = serverConnection;
     this.writer = writer;
+    messageQueue = new LinkedList<String>();
+  }
+
+  public synchronized void addMessageToQueue(String msg)
+  {
+    messageQueue.add(msg);
+    notify();
   }
 
   @Override
@@ -23,7 +36,28 @@ public class ServerWriter extends Thread
   {
     while(true)
     {
+      guardedWrite();
+      sendNextMessage();
+    }
+  }
 
+  private synchronized void sendNextMessage()
+  {
+    System.out.println(messageQueue.removeFirst());
+  }
+
+  private synchronized void guardedWrite()
+  {
+    while(messageQueue.isEmpty())
+    {
+      try
+      {
+        wait();
+      }
+      catch (InterruptedException ie)
+      {
+        ie.printStackTrace();
+      }
     }
   }
 }
