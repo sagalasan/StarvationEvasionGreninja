86a881afbb42e89506543e5bc584375da15b9db8
JJ505 <jjohnson101j@gmail.com>
2015-11-21 16:07:25 -0700

    moved methods initPane from swapToVoting/Policy pane methods in gui phase pane.  fixed issue where timer was a little wonky and reporting multiple timer results

--- src/starvationevasion/greninja/gui/GuiBase.java
+++ src/starvationevasion/greninja/gui/GuiBase.java
@@ -23,8 +23,9 @@ import starvationevasion.greninja.model.State;
 //TODO put whole gui on timeline.
 public class GuiBase extends Application
 {
-  private PolicyPane policyPane;
-  private VotingPane votingPane;
+  private PolicyPane policyPane = new PolicyPane(this);
+  private VotingPane votingPane = new VotingPane(this);
+
   private GamePhasePane activePane;
   private EnumRegion playerRegion, viewedRegion;
   private State playerRegionInfo, viewedRegionInfo; //references to region observed
@@ -33,6 +34,11 @@ public class GuiBase extends Application
   private GameController control;
   private GuiTimer guiTimer;
 
+  public void initializePanes()
+  {
+    votingPane.initPane();
+    policyPane.initPane();
+  }
   /*
   ===========================TO CONTROL=========================================
    */
@@ -77,8 +83,9 @@ public class GuiBase extends Application
   {
     System.out.println("Now in Voting.");
     //votingPane.getTimerPane().resetTimer();
-    votingPane = new VotingPane(this);
-    votingPane.initPane();
+    //votingPane = new VotingPane(this);
+    //votingPane.initPane();
+
     activePane = votingPane;
     baseScene.setRoot(votingPane);
   }
@@ -90,8 +97,8 @@ public class GuiBase extends Application
   {
     System.out.println("Now in Policy Drafting.");
     //policyPane.getTimerPane().resetTimer();
-    policyPane = new PolicyPane(this);
-    policyPane.initPane();
+    //policyPane = new PolicyPane(this);
+    //policyPane.initPane();
     activePane = policyPane;
     baseScene.setRoot(policyPane);
   }
@@ -105,6 +112,7 @@ public class GuiBase extends Application
     System.out.println("Entered Staging Pane");
     StagingPane staging = new StagingPane(this);
     staging.initPane();
+    //initializePanes();
     baseScene.setRoot(staging);
   }
 
@@ -195,6 +203,8 @@ public class GuiBase extends Application
   {
     //initialize components
     mainStage.setTitle("Starvation Evasion");
+
+    initializePanes();
     //policyPane = new PolicyPane(this);
     //policyPane.initPane();
     //votingPane = new VotingPane(this);
--- src/starvationevasion/greninja/gui/basePane/PolicyPane.java
+++ src/starvationevasion/greninja/gui/basePane/PolicyPane.java
@@ -4,7 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import starvationevasion.greninja.gui.GuiBase;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Base pane for Policy Drafting Phase.
@@ -14,36 +19,50 @@ public class PolicyPane extends GamePhasePane
 {
 
   private GuiBase base;
+  //private CardController ash;
 
   public PolicyPane(GuiBase base)
   {
     super(base);
     this.base = base;
+
   }
 
   /**
    * Instantiate and add components.
    */
-  @Override
+
   public void initPane()
   {
-    super.initPane();
+    super.initTimerPane();
     Button button = new Button();
     button.setText("Next State");
     button.setOnAction(new ButtonControl(this));
     getChildren().add(new Label("Policy Pane, draft policies"));
 
+    //Image image = new Image("file:assets/greninjaAssets/usMap.png");
+    //ash = new CardController(image, 50, 50, "ash");
+
     getChildren().add(getTimerPane());
+    //getChildren().add(ash.getCard());
 
     getChildren().add(button);
   }
+  //makes a new timer and replaces the old one
+  private void resetTimer()
+  {
+    int timerIndex = getChildren().indexOf(getTimerPane());
+    super.initTimerPane();
+    getChildren().set(timerIndex, getTimerPane());
 
+  }
   /**
    * Swap to voting pane
    * TODO will not need
    */
   public void endPhase()
   {
+    resetTimer();
     base.endPolicyDraftingPhase();
   }
   /**
