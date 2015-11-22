1d10213e9efb07c96c34d521b0ebeb1e4fa6190f
sagalasan <cmdm@unm.edu>
2015-11-22 12:30:52 -0700

    Added test server package and serverstate class

---
+++ src/starvationevasion/greninja/serverCom/ServerState.java
@@ -0,0 +1,16 @@
+package starvationevasion.greninja.serverCom;
+
+/**
+ * Created by sagalasan on 11/22/15.
+ */
+public class ServerState
+{
+  public enum ServerStateEnum
+  {
+    WAITING_FOR_LOGIN_RESPONSE,
+    WAITING_FOR_BEGIN_GAME_RESPONSE,
+    WAITING_FOR_RESPONSE
+  }
+
+
+}
---
+++ src/starvationevasion/greninja/serverCom/testServer/TestServer.java
@@ -0,0 +1,9 @@
+package starvationevasion.greninja.serverCom.testServer;
+
+/**
+ * Created by sagalasan on 11/22/15.
+ */
+public class TestServer
+{
+
+}
