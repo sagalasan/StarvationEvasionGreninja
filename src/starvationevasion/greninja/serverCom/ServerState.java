920a77101572d89ca92114df25d31e9e17e36cbc
sagalasan <cmdm@unm.edu>
2015-11-22 20:11:45 -0700

    Added enum to serverState

--- src/starvationevasion/greninja/serverCom/ServerState.java
+++ src/starvationevasion/greninja/serverCom/ServerState.java
@@ -5,12 +5,29 @@ package starvationevasion.greninja.serverCom;
  */
 public class ServerState
 {
+  private static volatile ServerState myInstance = null;
+  private ServerStateEnum state;
+
   public enum ServerStateEnum
   {
+    IDLE,
     WAITING_FOR_LOGIN_RESPONSE,
     WAITING_FOR_BEGIN_GAME_RESPONSE,
     WAITING_FOR_RESPONSE
   }
 
+  public synchronized static ServerState getInstance()
+  {
+    if(myInstance == null)
+    {
+      myInstance = new ServerState();
+    }
+    return myInstance;
+  }
+
+  private ServerState()
+  {
+    state = ServerStateEnum.IDLE;
+  }
 
 }
