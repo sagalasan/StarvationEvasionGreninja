461aee6ef21eac7ab8aaa6310427ab31ab319030
sagalasan <cmdm@unm.edu>
2015-11-28 14:05:24 -0700

    Added ServerConnectionPane class

---
+++ src/starvationevasion/greninja/gui/basePane/ServerConnectionPane.java
@@ -0,0 +1,16 @@
+package starvationevasion.greninja.gui.basePane;
+
+import javafx.scene.layout.StackPane;
+import starvationevasion.greninja.gui.GuiBase;
+
+/**
+ * Created by Christiaan Martinez on 11/28/15.
+ */
+public class ServerConnectionPane extends StackPane
+{
+  private GuiBase guiBase;
+  public ServerConnectionPane(GuiBase guiBase)
+  {
+    this.guiBase = guiBase;
+  }
+}
