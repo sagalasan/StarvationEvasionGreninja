66cd2caa2cf505260295f6231b398cfc39465bc7
sagalasan <cmdm@unm.edu>
2015-11-28 14:18:15 -0700

    Implemented partial implementation of ServerConnectionPane

--- src/starvationevasion/greninja/gui/basePane/ServerConnectionPane.java
+++ src/starvationevasion/greninja/gui/basePane/ServerConnectionPane.java
@@ -2,8 +2,15 @@ package starvationevasion.greninja.gui.basePane;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import starvationevasion.greninja.gui.GuiBase;
 
 /**
@@ -13,6 +20,8 @@ public class ServerConnectionPane extends StackPane
 {
   private GuiBase guiBase;
 
+  private GridPane serverBox;
+
   private Button connectButton;
   private Button cancelButton;
   private ButtonListener buttonListener;
@@ -20,6 +29,39 @@ public class ServerConnectionPane extends StackPane
   public ServerConnectionPane(GuiBase guiBase)
   {
     this.guiBase = guiBase;
+    buttonListener = new ButtonListener(this);
+    initGui();
+  }
+
+  public void initGui()
+  {
+    serverBox = new GridPane();
+    Rectangle rect    = new Rectangle(500, 500, Color.GRAY);
+
+    Button connectButton = new Button("Connect");
+    Button cancelButton  = new Button("Cancel");
+
+    TextField nameField = new TextField();
+    TextField portField = new TextField();
+    nameField.setMaxWidth(Double.MAX_VALUE);
+    portField.setMaxWidth(Double.MAX_VALUE);
+
+    connectButton.setOnAction(buttonListener);
+
+
+    serverBox.setAlignment(Pos.CENTER);
+    serverBox.setHgap(4);
+    serverBox.setVgap(4);
+    serverBox.setPadding(new Insets(25, 25, 25, 25));
+    serverBox.add(new Label("Please enter Server hostname and port."), 0, 0, 2, 1);
+    serverBox.add(new Label("Hostname: "), 0, 1);
+    serverBox.add(nameField, 1, 1);
+    serverBox.add(new Label("Port: "), 0, 2);
+    serverBox.add(portField, 1, 2);
+    serverBox.add(connectButton, 0, 3);
+    serverBox.add(cancelButton, 1, 3);
+    this.getChildren().add(rect);
+    this.getChildren().add(serverBox);
   }
 
   public void buttonPressed(ActionEvent event)
