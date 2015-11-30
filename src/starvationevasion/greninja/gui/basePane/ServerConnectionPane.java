02c8f2ad537274c757465eb6263515b4433997bf
sagalasan <cmdm@unm.edu>
2015-11-29 19:38:41 -0700

    Uses a timer to try and connect to server

--- src/starvationevasion/greninja/gui/basePane/ServerConnectionPane.java
+++ src/starvationevasion/greninja/gui/basePane/ServerConnectionPane.java
@@ -1,5 +1,6 @@
 package starvationevasion.greninja.gui.basePane;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -12,8 +13,10 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import starvationevasion.greninja.gui.GuiBase;
+import starvationevasion.greninja.serverCom.ServerConnection;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Christiaan Martinez on 11/28/15.
@@ -35,16 +38,24 @@ public class ServerConnectionPane extends StackPane
 
   private static final String INVALID_PORT_MSG = "This is an invalid port, please try again";
   private static final String UNABLE_CONNECT_MSG = "Could not connect to server, please try again";
-  private static final String ATTEMPT_CONNECT_MSG = "Attempting to connect to server...";
+  private static final String ATTEMPT_CONNECT_MSG = "Attempting to connect to server... ";
+  private static final String RETRY_CONNECT_MSG = "Could not connect, retrying";
+  private String firstAttemptMessage = ATTEMPT_CONNECT_MSG + "%ds";
+  private String retryAttepmtMessage = RETRY_CONNECT_MSG + " {%d/" + MAX_ATTEMPT_CONNECT + "} %ds";
 
   // Variables used to retry connections up to 4 times every 15 seconds
   private Timer connectToServerTimer;
   private int connectionCountTimeout;
-  private static final int MAX_ATTEMPT_CONNECT = 4;
-  private static final int CONNECTION_INTERVAL = 15000;
-  private static final int REFRESH_INTERVAL = 1000;
+
+  // public because inner class needs these
+  public static final int MAX_ATTEMPT_CONNECT = 4;
+  public static final long CONNECTION_INTERVAL = 15000;
+  private static final long REFRESH_INTERVAL = 1000;
 
   private boolean helloReceived = false;
+
+  private String hostName;
+  private int port;
 
   public ServerConnectionPane(GuiBase guiBase)
   {
@@ -91,14 +102,15 @@ public class ServerConnectionPane extends StackPane
   {
     Button source = (Button) event.getSource();
     System.out.println("Button pressed");
+
     if(source == connectButton)
     {
-      String hostName = nameField.getText();
+      String hostNameString = nameField.getText();
       String portString = portField.getText();
       int portInt = getPortInt(portString);
 
       System.out.println("Attempting to connect:");
-      System.out.println("\tHostname: " + hostName);
+      System.out.println("\tHostname: " + hostNameString);
       System.out.println("\tPort: " + portInt);
 
       if(portInt == -1)
@@ -108,8 +120,12 @@ public class ServerConnectionPane extends StackPane
       }
       else
       {
+        hostName = hostNameString;
+        port = portInt;
         responseMessageLabel.setText(ATTEMPT_CONNECT_MSG);
         responseMessageLabel.setTextFill(Color.WHITE);
+        connectToServerTimer = new Timer();
+        connectToServerTimer.schedule(new ScheduledTimer(this), 0, REFRESH_INTERVAL);
       }
     }
     else if(source == cancelButton)
@@ -134,6 +150,88 @@ public class ServerConnectionPane extends StackPane
     }
 
     return port;
+  }
+
+  public void sendConnection()
+  {
+    guiBase.attemptToConnectServer(hostName, port);
+  }
+
+  public void updateMessageStatus(long timeDiff, int currentAttempt)
+  {
+    int timeDisplay = (int) timeDiff / 1000;
+    if(currentAttempt == 0)
+    {
+      responseMessageLabel.setText(String.format(firstAttemptMessage, timeDisplay));
+    }
+    else
+    {
+      responseMessageLabel.setText(String.format(retryAttepmtMessage, currentAttempt, timeDisplay));
+    }
+  }
+
+  public void stopTimer()
+  {
+    responseMessageLabel.setText(UNABLE_CONNECT_MSG);
+    connectToServerTimer.cancel();
+  }
+
+  private class ScheduledTimer extends TimerTask
+  {
+    private ServerConnectionPane reference;
+
+    private boolean hasStarted = false;
+
+    private int maxAttempts;
+    private long attemptInterval;
+    private long attemptStart;
+    private int currentAttempt;
+
+    public ScheduledTimer(ServerConnectionPane reference)
+    {
+      this.reference = reference;
+      maxAttempts = ServerConnectionPane.MAX_ATTEMPT_CONNECT;
+      attemptInterval = ServerConnectionPane.CONNECTION_INTERVAL;
+      currentAttempt = 0;
+    }
+
+    @Override
+    public void run()
+    {
+      Platform.runLater(this::attemptConnection);
+    }
+
+    private void attemptConnection()
+    {
+      long timeDiff = System.currentTimeMillis() - attemptStart;
+      if(!hasStarted)
+      {
+        attemptStart = System.currentTimeMillis();
+        hasStarted = true;
+        sendConnection();
+        reference.updateMessageStatus(0, currentAttempt);
+      }
+      else
+      {
+        if(timeDiff > attemptInterval)
+        {
+          currentAttempt++;
+          if(currentAttempt > maxAttempts) reference.stopTimer();
+          else
+          {
+            attemptStart = System.currentTimeMillis();
+            reference.sendConnection();
+            reference.updateMessageStatus(0, currentAttempt);
+          }
+        }
+        else
+        {
+          reference.updateMessageStatus(timeDiff, currentAttempt);
+        }
+      }
+    }
+
+    private void sendConnection() { reference.sendConnection(); }
   }
 
   private class ButtonListener implements EventHandler<ActionEvent>
