1e66b3d8f5dc76d00a32813a5d54fbe96a5d14e9
esosebee <esosebee@unm.edu>
2015-12-01 02:58:30 -0700

    Changed data structure in AIDecisions

--- src/starvationevasion/greninja/gameControl/AIDecisions.java
+++ src/starvationevasion/greninja/gameControl/AIDecisions.java
@@ -2,7 +2,6 @@ package starvationevasion.greninja.gameControl;
 
 import starvationevasion.common.EnumRegion;
 import starvationevasion.common.PolicyCard;
-import starvationevasion.common.messages.AvailableRegions;
 import starvationevasion.greninja.model.AIPlayer;
 import starvationevasion.greninja.model.State;
 
@@ -10,19 +9,24 @@ import java.util.*;
 
 
 /**
- * Created by Erin on 11/27/15.
+ *
  */
 public class AIDecisions
 {
   private AIPlayer player;
-//  List<PolicyCard> playerHand;
   private int turnNumber;
+//  List<PolicyCard> playerHand;
+
   private State localRegion;
   private State worldRegion;
   private double regionPop, worldPop, localHDI, worldHDI;
+
   private double[] probabilities;
-  private HashMap<Integer, Integer> rankedCards = new HashMap<Integer, Integer>();
+  private LinkedHashMap<Integer, Integer> rankedCards = new LinkedHashMap<Integer, Integer>();
   private Random rand = new Random();
+
+  private boolean communication = false;
+  private PolicyCard suggestedCard;
 
   private boolean DEBUG = true;
 
@@ -35,7 +39,7 @@ public class AIDecisions
     rankedCards.put(30, 2);
     rankedCards.put(40, 5);
     rankedCards.put(50, 4);
-    chooseCard(rankedCards);
+    draftCard(rankedCards);
   }
 
   public AIDecisions(AIPlayer player, int turnNumber)
@@ -53,8 +57,21 @@ public class AIDecisions
     getCurrentInfo();
   }
 
+  /**
+   * Constructor for the AI player to use if another player is communicating with them.
+   * @param player        the AI player to control.
+   * @param turnNumber    the current turn.
+   * @param suggestedCard the card to consider voting for.
+   */
+  public AIDecisions(AIPlayer player, int turnNumber, PolicyCard suggestedCard)
+  {
+    this.communication = true;
+    this.suggestedCard = suggestedCard;
+  }
+
   private void initializeProbabilities()
   {
+    if (DEBUG) System.out.println("AIDecisions: initializeProbabilities()");
 //    probabilities = new double[player.getPlayerHand().size()];
     probabilities = new double[rankedCards.size()];
 
@@ -63,15 +80,12 @@ public class AIDecisions
     for (int probability : rankedCards.keySet())
     {
       probabilities[i] = probability;
+      if (DEBUG) System.out.println("probability[" + i + "]: " + probabilities[i]);
       s += probabilities[i];
       i++;
     }
-//    for (int i = 0; i < probabilities.length; i++)
-//    {
-////      probabilities[i] = rankedCards.
-//      s += probabilities[i];
-//    }
 
+    // Proportionate card selection
     if (s == 0)
     {
       s = 1 / probabilities.length;
@@ -106,17 +120,36 @@ public class AIDecisions
     else return false;
   }
 
+  public int voteCard(LinkedHashMap<Integer, Integer> rankedCards, PolicyCard suggestedCard)
+  {
+    // TODO: Works similar to draftCard but adds weight to suggestedCard?
+    return 0;
+  }
+
   /**
    * Chooses a card based on its effects on the game state. A card that benefits the player is more likely to be selected,
    * but it is not guaranteed.
    * @param rankedCards
    * @return the index of the card to be selected.
    */
-  public int chooseCard(HashMap<Integer, Integer> rankedCards)
+  public int draftCard(LinkedHashMap<Integer, Integer> rankedCards)
   {
+    if (DEBUG) System.out.println("AIDecisions: draftCard()");
+
+    if (DEBUG) for (int probability : rankedCards.keySet())
+    {
+      System.out.println("Rank: " + probability);
+    }
+
+    if (DEBUG) for (int index : rankedCards.values())
+    {
+      System.out.println("Index: " + index);
+    }
+
     // key = rank, value = index
     // Bigger rank number = higher probability
     if (probabilities == null) initializeProbabilities();
+    List<Integer> rankedCardsIndices = new ArrayList<Integer>(rankedCards.values());
 
     int selectedIndex = 0; // Initialize to zero
 
@@ -128,7 +161,8 @@ public class AIDecisions
       {
         selectedIndex = i;
         if (DEBUG) System.out.println("Selected index in HashMap: " + selectedIndex);
-        return selectedIndex;
+        if (DEBUG) System.out.println("Index of card to be chosen: " + rankedCardsIndices.get(selectedIndex));
+        return rankedCardsIndices.get(selectedIndex);
       }
     }
     if (DEBUG) System.out.println("Selected index: -1");
@@ -142,15 +176,15 @@ public class AIDecisions
    */
   public int analyzeCards(List<PolicyCard> cards)
   {
-    HashMap<Integer, Integer> rankedCards = new HashMap(); // <rank, index>  or <rank, card>?
+    LinkedHashMap<Integer, Integer> rankedCards = new LinkedHashMap<Integer, Integer>(); // <rank, index>  or <rank, card>?
     int numCards = cards.size();
     int chosenIndex = 0; // Zero by default
     for (int i = 0; i < numCards; i++)
     {
-      // rank cards
+      // Sort cards from best to worst
     }
 
-    chosenIndex = chooseCard(rankedCards);
+    chosenIndex = draftCard(rankedCards);
     return chosenIndex;
   }
 
